a94647c70b382b1b32fe9259218e2df6c7d6553b
zoeblighton <zoeblighton.seo@gmail.com>
2025-12-09 18:08:45 +0000

    App.js skeleton

--- src/App.js
+++ src/App.js
@@ -3,8 +3,45 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <h1>Zoe Blighton - Portfolio </h1>
-      <p>Junior Front-End Developer</p>
+      <header>
+        <div>
+          <h1>Zoe Blighton</h1>
+          <nav>
+            <a href="#about">About</a>
+            <a href="#projects">Projects</a>
+            <a href="#resume">Resume</a>
+            <a href="#contact">Contact</a>
+          </nav>
+        </div>
+      </header>
+
+      <main>
+        <section id="about">
+          <h2>About</h2>
+          <p>Short bio will go here.</p>
+        </section>
+
+        <section id="projects">
+          <h2>Personal Projects</h2>
+          <p>Project cards will go here.</p>
+        </section>
+
+        <section id="resume">
+          <h2>Resume</h2>
+          <p>Link to my resume will go here.</p>
+        </section>
+
+        <section id="contact">
+          <h2>Contact</h2>
+          <p>Send me an Email: zoeblighton.seo@gmail.com </p>
+          <p>or fill in the form below.</p>
+          <p>Contact form will go here.</p>
+        </section>
+      </main>
+
+      <footer>
+        <p> {new Date().getFullYear()} Coded by Zoe Blighton</p>
+      </footer>
     </div>
   );
 }
